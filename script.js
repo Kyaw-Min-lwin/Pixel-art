1ac973b0dc08293691e889c0b92c545c2e6cb347
Kyaw Min Lwin <kyawminlwinprogrammer@gmail.com>
2021-09-15 00:56:21 +0630

    Added the html and css[incomplete] and some js

---
+++ script.js
@@ -0,0 +1,6 @@
+let colorPicker = document.querySelector(`input[type="color"]`);
+let colorPickerWrapper = document.querySelector(".color-picker-wrapper");
+colorPicker.onchange = function () {
+    colorPickerWrapper.style.backgroundColor = colorPicker.value;
+}
+colorPickerWrapper.style.backgroundColor = colorPicker.value;
